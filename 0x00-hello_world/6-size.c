47c7fbb3276e788cf5a05d7fe2a2646bbe1ddc25
MvuviTech <africanboi0@gmail.com>
2022-10-16 00:34:09 -0700

    Data type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 8 bytes(s)\n");
+	printf("Size of long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+	
+}
