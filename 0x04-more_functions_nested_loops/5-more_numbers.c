14f1c3f2a38e05967ae97cd3a1d336ed7a3c4f6d
MvuviTech <africanboi0@gmail.com>
2022-10-21 19:08:41 -0700

    Print More Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- *  print_most_numbers -  a function that prints the numbers, from 0 to 9,
+ * more_numbers -  a function that prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
- *  except 2 and 4
- *  Return: Always 0
+ * Return: Always 0
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	int a;
+	int b = 0;
 
-	for (a = '0'; a <= '9'; a++)
+	for (; b < 10; b++)
 	{
-		if (a != '2' && a != '4')
-			_putchar(a);
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
